54bc0caec0bbf699f992e225ea9ff2e21e01b282
GFI System <artist@gfisystem.com>
2025-03-06 09:58:59 +0700

    batch encoding convert

---
+++ solis-ventus/manual/convert.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+iconv -f windows-1252 -t utf-8 index_files/Page268.htm > Page268.htm
+iconv -f windows-1252 -t utf-8 index_files/Page297.htm > Page297.htm
+iconv -f windows-1252 -t utf-8 index_files/Page306.htm > Page306.htm
+iconv -f windows-1252 -t utf-8 index_files/Page329.htm > Page329.htm
+iconv -f windows-1252 -t utf-8 index_files/Page380.htm > Page380.htm
+iconv -f windows-1252 -t utf-8 index_files/Page412.htm > Page412.htm
+iconv -f windows-1252 -t utf-8 index_files/Page444.htm > Page444.htm
+iconv -f windows-1252 -t utf-8 index_files/Page462.htm > Page462.htm
+iconv -f windows-1252 -t utf-8 index_files/Page485.htm > Page485.htm
+iconv -f windows-1252 -t utf-8 index_files/Page618.htm > Page618.htm
+iconv -f windows-1252 -t utf-8 index_files/Page728.htm > Page728.htm
+iconv -f windows-1252 -t utf-8 index_files/Page793.htm > Page793.htm
+iconv -f windows-1252 -t utf-8 index_files/Page812.htm > Page812.htm
+iconv -f windows-1252 -t utf-8 index_files/Page817.htm > Page817.htm
+iconv -f windows-1252 -t utf-8 index_files/Page861.htm > Page861.htm
+iconv -f windows-1252 -t utf-8 index_files/Page875.htm > Page875.htm
+iconv -f windows-1252 -t utf-8 index_files/Page940.htm > Page940.htm
+iconv -f windows-1252 -t utf-8 index_files/Page986.htm > Page986.htm
+iconv -f windows-1252 -t utf-8 index_files/Page1035.htm > Page1035.htm
+iconv -f windows-1252 -t utf-8 index_files/Page1115.htm > Page1115.htm
+iconv -f windows-1252 -t utf-8 index_files/Page1120.htm > Page1120.htm
+iconv -f windows-1252 -t utf-8 index_files/Page1144.htm > Page1144.htm
+iconv -f windows-1252 -t utf-8 index_files/Page1185.htm > Page1185.htm
+iconv -f windows-1252 -t utf-8 index_files/Page1294.htm > Page1294.htm
+iconv -f windows-1252 -t utf-8 index_files/Page1302.htm > Page1302.htm
+iconv -f windows-1252 -t utf-8 index_files/Page1343.htm > Page1343.htm
+iconv -f windows-1252 -t utf-8 index_files/Page1723.htm > Page1723.htm
+iconv -f windows-1252 -t utf-8 index_files/Page1740.htm > Page1740.htm
+
+echo "Conversion complete!"
